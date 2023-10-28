8a779fc50de8422ebdc266ee20c5c3477e03475b
Michael Novas <centfifty70@yahoo.com>
2023-10-28 14:50:36 -0400

    Initial Commit

---
+++ app/(components)/TicketCard.jsx
@@ -0,0 +1,32 @@
+import DeleteBlock from "./DeleteBlock";
+import PriorityDisplay from "./PriorityDisplay";
+import ProgressDisplay from "./ProgressDisplay";
+import StatusDisplay from "./StatusDisplay";
+
+const TicketCard = () => {
+  return (
+    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
+        <PriorityDisplay />
+        <div className="ml-auto">
+          <DeleteBlock />
+        </div>
+      </div>
+      <h4>Ticket Title</h4>
+      <hr className="h-px border-0 bg-page mb-2"></hr>
+      <p className="whitespace-pre-wrap">Ticket Description! Do This Ticket!</p>
+      <div className="flex-grow"></div>
+      <div className="flex mt-2 ">
+        <div className="flex flex-col">
+          <p className="text-xs my-1"> 10/28/23 12:12PM</p>
+          <ProgressDisplay />
+        </div>
+        <div className="ml-auto flex items-end">
+          <StatusDisplay />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TicketCard;
